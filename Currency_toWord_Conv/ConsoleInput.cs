e3e9eef585d6a71eb0826d03fbb404e869685e09
al24 <andrew.laptev24@gmail.com>
2020-07-11 11:48:43 +0300

    initial commit

---
+++ Currency_toWord_Conv/ConsoleInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Currency_toWord_Conv
+{
+    class ConsoleInput
+    {
+        public void GetNumber()
+        {
+            Conventer convert = new Conventer();
+
+            string answer = "N";
+            do
+            {
+                Console.Write("Enter a number: ");
+                double numInput = double.Parse(Console.ReadLine());
+                Console.WriteLine(convert.changeCurrencyToWords(numInput));
+
+                Console.WriteLine("Press <Y> to try again, or <N> to exit...");
+                answer = Console.ReadLine().ToUpper();
+            }
+            while (answer == "Y");
+
+            if (answer == "N")
+            {
+                Console.WriteLine("The app close after 3 seconds...");
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
---
+++ Currency_toWord_Conv/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Currency_toWord_Conv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleInput input = new ConsoleInput();
+
+            input.GetNumber();
+        }
+    }
+}
